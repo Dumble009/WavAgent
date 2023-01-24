db5fe8be530cacae0b13b20debef2c76e3656475
Dumble009 <daichi19990821@gmail.com>
2023-01-24 21:58:52 +0900

    initial commit プロジェクトの初期設定を済ませ、ビルドが出来る状態。

---
+++ sources/main.cpp
@@ -0,0 +1,9 @@
+﻿// ----------STLのinclude----------
+#include <iostream> // 入出力
+#include "test_lib.h"
+
+int main(void)
+{
+    std::cout << "Hello World" << std::endl;
+    std::cout << "1 + 2 = " << testLib::add(1, 2) << std::endl;
+}
---
+++ tests/sources/hello_test.cpp
@@ -0,0 +1,16 @@
+﻿#include <gtest/gtest.h>
+#include "test_lib.h"
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions)
+{
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(LinkTest, BasicAssertions)
+{
+    EXPECT_EQ(1 + 2, testLib::add(1, 2));
+}
