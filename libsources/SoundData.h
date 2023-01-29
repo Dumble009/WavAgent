a2e973fdfb22f2d0b2c98163ba6bc77ec06e689e
Dumble009 <daichi19990821@gmail.com>
2023-01-29 23:29:15 +0900

    音声ファイルから読み込んだデータを保持するクラス SoundDataの定義を作成

---
+++ libsources/SoundData.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <vector>
+#include "MetaData.h"
+#include "SampleDataTypeDefinitions.h"
+#include "WavAgentErrorCode.h"
+
+namespace wavAgent
+{
+    // 音声ファイルから読み込まれたデータを保持するクラス。
+    class SoundData
+    {
+    private:
+        // 波形データ。各配列が各チャンネルの波形を表す
+        std::vector<std::vector<unsigned char>> waves;
+
+        // サンプリング周波数などのメタ情報
+        MetaData metaData;
+
+    public:
+        // 特定のチャンネルの波形のポインタを返す
+        WavAgentErrorCode GetWave(SampleUnsigned8bit **ppWave,
+                                  int channel);
+
+        // 特定のチャンネルの波形のポインタを返す
+        WavAgentErrorCode GetWave(SampleSigned16bit **ppWave,
+                                  int channel);
+
+        // 特定のチャンネルの波形のポインタを返す
+        WavAgentErrorCode GetWave(SampleSigned24bit **ppWave,
+                                  int channel);
+
+        // 特定のチャンネルの波形のポインタを返す
+        WavAgentErrorCode GetWave(SampleSigned32bit **ppWave,
+                                  int channel);
+
+        // 特定のチャンネルの波形のポインタを返す
+        WavAgentErrorCode GetWave(SampleSigned32bitFloat **ppWave,
+                                  int channel);
+
+        // 音声データに含まれるメタデータを保持したオブジェクトへのポインタを返す
+        WavAgentErrorCode GetMetaData(MetaData **ppMetaData);
+    };
+}
