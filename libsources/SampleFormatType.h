7e041a87ebdf2ddeea286533d29db0e4138e5725
Dumble009 <daichi19990821@gmail.com>
2023-01-29 22:38:04 +0900

    波形データのサンプルのデータ形式を表すenum, SampleFormatTypeのヘッダファイルを実装

---
+++ libsources/SampleFormatType.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+namespace wavAgent
+{
+    // サンプルのデータ形式。ビット深度と、符号の有無等
+    enum class SampleFormatType : int
+    {
+        WAV_AGENT_SAMPLE_STRUCTURE_UNSIGNED_8_BIT = 0,
+        WAV_AGENT_SAMPLE_STRUCTURE_SIGNED_16_BIT = 1,
+        WAV_AGENT_SAMPLE_STRUCTURE_SIGNED_24_BIT = 2,
+        WAV_AGENT_SAMPLE_STRUCTURE_SIGNED_32_BIT = 3,
+        WAV_AGENT_SAMPLE_STRUCTURE_SIGNED_32_BIT_FLOAT = 4,
+    };
+}
