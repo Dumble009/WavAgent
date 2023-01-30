2fd7ea75a791fc4d164d3380219bb509abb8d87c
Dumble009 <daichi19990821@gmail.com>
2023-01-30 20:56:01 +0900

    波形データの読み込みテストをWavAgentTestに追加。 また、Load関数やSoundDage.GetWave関数の説明をREADME.mdに追加

--- README.md
+++ README.md
@@ -20,14 +20,14 @@ Load("data/sound.wav", &soundData);
 `SoundData` に含まれる、あるチャンネルの波形を取得するためには、 `GetWave` 関数を使用する。チャンネルは 0-indexed に指定する。なので、チャンネル 1 の波形を取得するためには、0 番の波形データを取得する。波形データは要求する形式の配列先頭のポインタとして渡される。以下の例であれば、符号付き 16bit の波形データのチャンネル 1 を要求している。
 
 ```C++
-SampleSigned16bit *pWave;
+SampleSigned16bit *pWave = nullptr;
 soundData.GetWave(&pWave, 0);
 ```
 
 サウンドのチャンネル数やサンプリング周波数などのメタデータを取得する場合は、 `SoundData` が保持する `MetaData` を参照する。 `WavAgent` から `SoundData` を取得した時と同様に `MetaData` のポインタのポインタを `SoundData` の `GetMetaData` に渡すと、そのポインタが `SoundData` が保持する `MetaData` を指すようになる。
 
 ```C++
-MetaData *pMetaData = NULL;
+MetaData *pMetaData = nullptr;
 soundData.GetMetaData(&pMetaData);
 ```
 
@@ -56,3 +56,53 @@ ConvertWavAgentErrorCodeToDescription(WavAgentErrorCode::WAV_AGENT_SUCCESS)
  << std::endl;
 // WavAgent : Operation successfully completed. と出力される。
 ```
+
+# Load
+
+## 概要
+
+渡されたパスが指す wav ファイルを読み込み、引数に渡された `SoundData` のポインタの指す先に波形データなどをロードする。
+
+## 引数
+
+### path : `const std::string&`
+
+読み込み対象の wav ファイルのパス。相対パスでも絶対パスでも良い。
+
+### pSoundData : `SoundData*`
+
+wav ファイルから読み込んだデータを格納する先を示すポインタ。呼び出し側でオブジェクトを作成しておき、そのポインタを渡す必要がある。
+
+# SoundData
+
+wav ファイルから読み込まれたデータを保持するオブジェクト。波形データを保持し、メタデータを保持する `MetaData` オブジェクトを有する。
+
+## GetWave
+
+波形データを取得する。波形のサンプルのフォーマットに応じて、 `SampleUnsigned8bit`, `SampleSigned16bit`, `SampleSigned24bit`, `SampleSigned32bit`, `SampleSigned32bitFloat` それぞれとして波形データを読み込む関数を 5 つオーバーロードしている。
+
+### 引数
+
+#### ppWave : `SampleUnsigned8bit, SampleSigned16bit,...`
+
+波形データのポインタのポインタ。 `SampleUnsigned8bit` など対応するフォーマットのポインタのポインタを渡すと、そのポインタが指す先が `SoundData` 内の波形データになる。
+
+```c++
+SampleUnsigned8bit *pWave=nullptr
+soundData.GetWave(&pWave, 0);
+```
+
+`SoundData` が破棄されると、波形データも破棄されてしまうので、寿命に注意。
+要求するチャンネルやフォーマットが不適切な場合は `nullptr` を指すようになる。
+
+#### channel : int
+
+読み込み対象の波形データのチャンネル。0-indexed なので、チャンネル 1 のデータを読み込む場合は 0 を指定する。
+
+### 返り値 : `WavAgentErrorCode`
+
+正常に終了したら `WAV_AGENT_SUCCESS` を返す。
+
+存在しないチャンネルのデータを読み込もうとすると `WAV_AGENT_CHANNEL_OUT_OF_RANGE` を返す。
+
+wav ファイルが提供するフォーマットとは異なるフォーマットの `ppWave` を渡した場合は、 `WAV_AGENT_INVALID_FORMAT` を返す。
--- tests/sources/WavAgentTest.cpp
+++ tests/sources/WavAgentTest.cpp
@@ -40,12 +40,97 @@ void LoadAndCheckWaveData(const std::string &path,
 
     EXPECT_EQ(ret, wavAgent::WavAgentErrorCode::WAV_AGENT_SUCCESS);
 
+    // 各チャンネルの波形を独立に調べる
+    T *pWave = nullptr;
     for (int i = 0; i < channelCount; i++)
     {
+        ret = soundData.GetWave(&pWave, i);
+
+        EXPECT_EQ(ret, wavAgent::WavAgentErrorCode::WAV_AGENT_SUCCESS);
+        EXPECT_NE(pWave, nullptr); // pWaveには波形データを指す有効なポインタが格納される
+
+        // サンプルの値を一つずつ調べていく。
+        // 周期ごとに10個のサンプルが含まれ、前半5個が最大値、後半5個が最小値
+        int sampleIdx = 0;
+        for (int j = 0; j < waveCount; j++)
+        {
+            EXPECT_EQ(pWave[sampleIdx], maxValue);
+            EXPECT_EQ(pWave[sampleIdx + 1], maxValue);
+            EXPECT_EQ(pWave[sampleIdx + 2], maxValue);
+            EXPECT_EQ(pWave[sampleIdx + 3], maxValue);
+            EXPECT_EQ(pWave[sampleIdx + 4], maxValue);
+
+            EXPECT_EQ(pWave[sampleIdx + 5], minValue);
+            EXPECT_EQ(pWave[sampleIdx + 6], minValue);
+            EXPECT_EQ(pWave[sampleIdx + 7], minValue);
+            EXPECT_EQ(pWave[sampleIdx + 8], minValue);
+            EXPECT_EQ(pWave[sampleIdx + 9], minValue);
+
+            sampleIdx += 10;
         }
+    }
+
+    EXPECT_NE(pWave, nullptr) // 直前に読み込んだ波形を指している事を確認
+
+    // 存在しないチャンネルの波形データを読み込もうとするとエラーが変えることを調べる。
+    ret = soundData.GetWave(&pWave, channelCount + 1);
+
+    EXPECT_EQ(ret, wavAgent::WavAgentErrorCode::WAV_AGENT_CHANNEL_OUT_OF_RANGE);
+    EXPECT_EQ(pWave, nullptr); // 上でnullptrで無いことを確認したものがnullptrになっている事を確認
 }
+
+const std::string PATH_u8_1ch_4410 = "WAV-44100Hz-1ch-u8bit-4410Hz_square.wav";
+const std::string PATH_16_2ch_800 = "WAV-8000Hz-2ch-16bit-800Hz_square.wav";
+const std::string PATH_u8_1ch_38400 = "WAV-384000Hz-1ch-u8bit-38400Hz_square.wav";
+const std::string PATH_16_1ch_1600 = "WAV-44100Hz-1ch-16bit-4410Hz_square.wav";
+const std::string PATH_24_1ch_1600 = "WAV-16000Hz-1ch-24bit-1600Hz_square.wav";
+const std::string PATH_32_1ch_1600 = "WAV-16000Hz-1ch-32bit-1600Hz_square.wav";
+const std::string PATH_f32_1ch_1600 = "WAV-16000Hz-1ch-f32bit-16000Hz_square.wav";
 
 // 読み込んだ波形データが正しいかどうか
 TEST(WavAgentWavDataTest, BasicAssertions)
 {
+    LoadAndCheckWaveData(PATH_u8_1ch_4410,
+                         (wavAgent::SampleUnsigned8bit)0xFF,
+                         (wavAgent::SampleUnsigned8bit)0x00,
+                         1,
+                         4410);
+
+    LoadAndCheckWaveData(PATH_16_2ch_800,
+                         (wavAgent::SampleSigned16bit)0x7FFF,
+                         (wavAgent::SampleSigned16bit)0x8000,
+                         2,
+                         800);
+
+    LoadAndCheckWaveData(PATH_u8_1ch_38400,
+                         (wavAgent::SampleUnsigned8bit)0xFF,
+                         (wavAgent::SampleUnsigned8bit)0x00,
+                         1,
+                         38400);
+
+    LoadAndCheckWaveData(PATH_24_1ch_1600,
+                         (wavAgent::SampleSigned24bit)0x7FFFFF,
+                         (wavAgent::SampleSigned24bit)0x800000,
+                         1,
+                         1600);
+
+    LoadAndCheckWaveData(PATH_32_1ch_1600,
+                         (wavAgent::SampleSigned32bit)0x7FFFFFFF,
+                         (wavAgent::SampleSigned32bit)0x80000000,
+                         1,
+                         1600);
+
+    int maxValueInt = 0x3F800000; // floatの最大値の16進数表記
+    int minValueInt = 0xBF800000; // floatの最小値の16進数表記
+
+    // 浮動小数点に16進数表記をそのまま代入する事は出来ないので、
+    // 一度intに代入してから、ビット配列はそのままにfloatとして読み替える
+    wavAgent::SampleSigned32bitFloat maxValueFloat = *((wavAgent::SampleSigned32bitFloat *)&maxValueInt);
+    wavAgent::SampleSigned32bitFloat minValueFloat = *((wavAgent::SampleSigned32bitFloat *)&minValueInt);
+
+    LoadAndCheckWaveData(PATH_f32_1ch_1600,
+                         maxValueFloat,
+                         minValueFloat,
+                         1,
+                         1600);
 }
