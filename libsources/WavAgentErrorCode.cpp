92f0213486aadb82b014972f85cc1e830adfb277
Dumble009 <daichi19990821@gmail.com>
2023-02-02 22:27:12 +0900

    WavAgentErrorCode関連の関数のモック実装を作成

---
+++ libsources/WavAgentErrorCode.cpp
@@ -0,0 +1,19 @@
+﻿#include "WavAgentErrorCode.h"
+
+namespace wavAgent
+{
+    std::ostream &operator<<(std::ostream &os, WavAgentErrorCode code)
+    {
+        return os;
+    }
+
+    std::string ConvertWavAgentErrorCodeToString(WavAgentErrorCode code)
+    {
+        return "";
+    }
+
+    std::string GetDescriptionOfErrorCode(WavAgentErrorCode code)
+    {
+        return "";
+    }
+}
