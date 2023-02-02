a13027759153062623c1685be78dcacfae0563d5
Dumble009 <daichi19990821@gmail.com>
2023-02-02 22:21:05 +0900

    SoundDataのモック実装を作成

---
+++ libsources/SoundData.cpp
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include "SoundData.h"
+
+namespace wavAgent
+{
+    SoundData::SoundData() : waves(),
+                             metaData(0,
+                                      0,
+                                      SampleFormatType::WAV_AGENT_SAMPLE_STRUCTURE_UNSIGNED_8_BIT,
+                                      0)
+    {
+    }
+
+    WavAgentErrorCode SoundData::GetWave(
+        SampleUnsigned8bit **ppWave,
+        int channel)
+    {
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
+    }
+
+    WavAgentErrorCode SoundData::GetWave(
+        SampleSigned16bit **ppWave,
+        int channel)
+    {
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
+    }
+
+    WavAgentErrorCode SoundData::GetWave(
+        SampleSigned24bit **ppWave,
+        int channel)
+    {
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
+    }
+
+    WavAgentErrorCode SoundData::GetWave(
+        SampleSigned32bit **ppWave,
+        int channel)
+    {
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
+    }
+
+    WavAgentErrorCode SoundData::GetWave(
+        SampleSigned32bitFloat **ppWave,
+        int channel)
+    {
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
+    }
+
+    WavAgentErrorCode SoundData::GetMetaData(MetaData **ppMetaData)
+    {
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
+    }
+}
