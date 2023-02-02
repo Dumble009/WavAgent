fd7a4a7414d04f06dcbca5a92ca2b78981bc6f46
Dumble009 <daichi19990821@gmail.com>
2023-02-02 22:24:24 +0900

    WavAgent.Loadのモック実装を作成

--- libsources/WavAgent.cpp
+++ libsources/WavAgent.cpp
@@ -1,1 +1,11 @@
-﻿
+﻿#include "WavAgent.h"
+
+namespace wavAgent
+{
+    WavAgentErrorCode Load(
+        const std::string &path,
+        SoundData *pSoundData)
+    {
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
+    }
+}
