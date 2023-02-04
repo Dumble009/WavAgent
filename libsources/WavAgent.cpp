346b6a641b2f2eec69b5a1737c41ad34fc400c44
Dumble009 <daichi19990821@gmail.com>
2023-02-04 16:02:13 +0900

    WavAgentTest.cppのWavAgentLoadTestが通るようにLoad関数を実装
    ファイルをifstreamとして読み込み、存在するファイルかどうか、そのファイルの中身がwavファイルとして正しいかどうかを調べる処理を実装した。
    まだ、その他の処理は実装できていない。

--- libsources/WavAgent.cpp
+++ libsources/WavAgent.cpp
@@ -15,25 +15,86 @@ namespace wavAgent
     // サブチャンク識別子(data)。リトルエンディアンなのでdが下のバイトに来る
     constexpr int32_t DATA = 0x61746164;
 
-    // ifstreamから4バイト分のデータを読み込んで、int32_tとして返す
-    int32_t get32bitWord(
-        std::ifstream &ifs)
+    // ifstreamから4バイト分のデータを読み込んで、retValに代入する
+    WavAgentErrorCode get32bitWord(
+        std::ifstream &ifs,
+        int32_t *retVal)
     {
-        int32_t retVal = 0;
 
-        ifs.read((char *)&retVal, 4); // readはchar*しか受け取れないので、キャストする必要がある
+        ifs.read((char *)retVal, 4); // readはchar*しか受け取れないので、キャストする必要がある
 
-        return retVal;
+        // readで何か失敗が起こった場合はbadがtrueになるので、正常に読み込むことが出来たか確認する
+        if (ifs.bad())
+        {
+            return WavAgentErrorCode::WAV_AGENT_FILE_IS_BROKEN;
+        }
+
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
+    }
+
+    // ifsが保持するストリームが正しいwavファイルの物かを調べる。
+    WavAgentErrorCode checkWavFile(std::ifstream &ifs)
+    {
+        int32_t buffer32 = 0; // 32bit分のバッファ。ここにifsの先頭4バイトを貯めていく
+
+        // RIFFFタグを探す
+        bool isRFIFFound = false;
+        while (!ifs.eof())
+        {
+            // buffer32を1バイト分下にずらし、上のバイトにifsの先頭のバイトを読み込む
+            // 上のバイトを先頭にしているのは、ファイルの先頭のバイトがbuffer32の先頭のバイトに来るようにするため
+            buffer32 >>= 8;
+            ifs.read(((char *)&buffer32 + 3), 1);
+
+            if (buffer32 == RIFF)
+            {
+                isRFIFFound = true;
+                break;
+            }
+        }
+
+        // RFIFチャンクを持っていなかったら、そのファイルはwavファイルではない
+        if (!isRFIFFound)
+        {
+            return WavAgentErrorCode::WAV_AGENT_NOT_WAV_FILE;
+        }
+
+        // RFIFチャンクのフォーマットがWAVEである事を調べる。
+        ifs.read((char *)&buffer32, 4); // RFIFチャンクの識別子の次の4バイトはチャンクサイズ
+        ifs.read((char *)&buffer32, 4); // チャンクサイズの次の4バイトがフォーマット
+
+        // RFIFチャンクは必ず8バイトのデータを含んでいるはずなので、
+        // そうでなかったらファイルは破損している
+        if (ifs.bad())
+        {
+            return WavAgentErrorCode::WAV_AGENT_FILE_IS_BROKEN;
+        }
+
+        // フォーマットがWAVEで無かったら、そのファイルはwavファイルではない。
+        if (buffer32 != WAVE)
+        {
+            return WavAgentErrorCode::WAV_AGENT_NOT_WAV_FILE;
+        }
+
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
     }
 
     WavAgentErrorCode Load(
         std::string_view path,
         SoundData *pSoundData)
     {
+        // ファイルを読み込み、正常に読めるかどうか確認
         std::ifstream ifs(path.data(), std::ios::in | std::ios::binary);
-        if (!ifs.is_open())
+        if (ifs.fail())
         {
             return WavAgentErrorCode::WAV_AGENT_FILE_NOT_EXIST;
+        }
+
+        // 読み込んだファイルがwavファイルかどうかチェック
+        auto result = checkWavFile(ifs);
+        if (!IsWavAgentActionSucceeded(result))
+        {
+            return result;
         }
 
         return WavAgentErrorCode::WAV_AGENT_SUCCESS;
