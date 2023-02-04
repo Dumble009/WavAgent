cfd4e9d2f51ae27fd63fbfb3c7f40c2de3f79b3b
Dumble009 <daichi19990821@gmail.com>
2023-02-04 21:52:22 +0900

    float32のファイルを読み込む際に、文法エラーを組み込んでしまったので修正

--- tests/sources/WavAgentTest.cpp
+++ tests/sources/WavAgentTest.cpp
@@ -11,9 +11,8 @@ TEST(WavAgentLoadTest, BasicAssertions)
     EXPECT_EQ(ret, wavAgent::WavAgentErrorCode::WAV_AGENT_SUCCESS);
 
     // float32のファイルは他とは違うフォーマットタイプを持っているが、正常に読み込めることを確認
-    wavAgent::SoundData soundData = wavAgent::SoundData();
-    auto ret = wavAgent::Load("data/WAV-16000Hz-1ch-f32bit-16000Hz_square.wav",
-                              &soundData);
+    ret = wavAgent::Load("data/WAV-16000Hz-1ch-f32bit-16000Hz_square.wav",
+                         &soundData);
     EXPECT_EQ(ret, wavAgent::WavAgentErrorCode::WAV_AGENT_SUCCESS);
 
     // 存在しないファイルを読み込み、エラーが返ってくることを確かめる
