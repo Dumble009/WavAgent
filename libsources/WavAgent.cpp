64864fd5c48644c121d549c4016c61bf721b8198
Dumble009 <daichi19990821@gmail.com>
2023-02-04 20:16:38 +0900

    WavAgent.cpp内で、ifstreamから特定のバイト列を探索する処理を関数として実装
    RIFFやfmtなど、特定のバイト列を入力から探索する処理は頻出するので、任意の長さのバイト列探索することが出来る関数findIdentifierを実装した。

--- libsources/WavAgent.cpp
+++ libsources/WavAgent.cpp
@@ -15,13 +15,13 @@ namespace wavAgent
     // サブチャンク識別子(data)。リトルエンディアンなのでdが下のバイトに来る
     constexpr int32_t DATA = 0x61746164;
 
-    // ifstreamから4バイト分のデータを読み込んで、retValに代入する
+    // ifstreamから4バイト分のデータを読み込んで、pRetValの先に代入する
     WavAgentErrorCode get32bitWord(
         std::ifstream &ifs,
-        int32_t *retVal)
+        int32_t *pRetVal)
     {
 
-        ifs.read((char *)retVal, 4); // readはchar*しか受け取れないので、キャストする必要がある
+        ifs.read((char *)pRetVal, sizeof(int32_t)); // readはchar*しか受け取れないので、キャストする必要がある
 
         // readで何か失敗が起こった場合はbadがtrueになるので、正常に読み込むことが出来たか確認する
         if (ifs.bad())
@@ -32,36 +32,64 @@ namespace wavAgent
         return WavAgentErrorCode::WAV_AGENT_SUCCESS;
     }
 
+    // ifstreamから2バイト分のデータを読み込んで、pRetValに代入する
+    WavAgentErrorCode get16bitWord(
+        std::ifstream &ifs,
+        int16_t *pRetVal)
+    {
+        ifs.read((char *)pRetVal, sizeof(int16_t)); // readはchar*しか受け取れないので、キャストする必要がある
+
+        // readで何か失敗が起こった場合はbadがtrueになるので、正常に読み込むことが出来たか確認する
+        if (ifs.bad())
+        {
+            return WavAgentErrorCode::WAV_AGENT_FILE_IS_BROKEN;
+        }
+
+        return WavAgentErrorCode::WAV_AGENT_SUCCESS;
+    }
+
+    // ifsを読み込み、identifierが見つかればtrue、見つからなければfalseを返す
+    // 関数から帰った後のifsは、identifierを読み終わった直後の状態になっている
+    template <class T>
+    bool findIdentifier(
+        std::ifstream &ifs,
+        T identifier)
+    {
+        T buffer{}; // ここにifsから読み込んだ最新のsizeof(T)バイトのデータを読み込む
+
+        bool isIdentifierFound = false;
+        while (!ifs.eof())
+        {
+            // bufferを1バイト分下にずらし、上のバイトにifsの先頭のバイトを読み込む
+            // 上のバイトに最新の値を入れるのは、変数はメモリ上でリトルエンディアンになっているので、
+            // ファイルの先頭にあるバイトがbufferの先頭のバイトに来るようにするため
+            buffer >>= 8;
+            ifs.read((char *)&buffer + (sizeof(T) - 1), 1);
+
+            if (buffer == identifier)
+            {
+                isIdentifierFound = true;
+                break;
+            }
+        }
+
+        return isIdentifierFound;
+    }
+
     // ifsが保持するストリームが正しいwavファイルの物かを調べる。
     WavAgentErrorCode checkWavFile(std::ifstream &ifs)
     {
         int32_t buffer32 = 0; // 32bit分のバッファ。ここにifsの先頭4バイトを貯めていく
 
         // RIFFFタグを探す
-        bool isRFIFFound = false;
-        while (!ifs.eof())
-        {
-            // buffer32を1バイト分下にずらし、上のバイトにifsの先頭のバイトを読み込む
-            // 上のバイトを先頭にしているのは、ファイルの先頭のバイトがbuffer32の先頭のバイトに来るようにするため
-            buffer32 >>= 8;
-            ifs.read(((char *)&buffer32 + 3), 1);
-
-            if (buffer32 == RIFF)
-            {
-                isRFIFFound = true;
-                break;
-            }
-        }
-
-        // RFIFチャンクを持っていなかったら、そのファイルはwavファイルではない
-        if (!isRFIFFound)
+        if (!findIdentifier(ifs, RIFF))
         {
             return WavAgentErrorCode::WAV_AGENT_NOT_WAV_FILE;
         }
 
         // RFIFチャンクのフォーマットがWAVEである事を調べる。
-        ifs.read((char *)&buffer32, 4); // RFIFチャンクの識別子の次の4バイトはチャンクサイズ
-        ifs.read((char *)&buffer32, 4); // チャンクサイズの次の4バイトがフォーマット
+        get32bitWord(ifs, &buffer32); // RFIFチャンクの識別子の次の4バイトはチャンクサイズ
+        get32bitWord(ifs, &buffer32); // チャンクサイズの次の4バイトがフォーマット
 
         // RFIFチャンクは必ず8バイトのデータを含んでいるはずなので、
         // そうでなかったらファイルは破損している
