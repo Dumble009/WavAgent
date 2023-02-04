3a76a079499bdca8793a9da30bea5efdedf360f3
Dumble009 <daichi19990821@gmail.com>
2023-02-04 13:24:01 +0900

    WavAgentErrorCodeを文字列に変換する系の関数の返り値をstringからstring_viewに変更
    変換結果の文字列の配列をstring_viewで持ちたいが、返り値をstringにすると、stringへのキャストが毎回生じる。
    それでは効率が悪いので、返り値をstring_viewに変えて、そのまま返せるようにした。
    string_viewからstringへの変換は暗黙的には出来ないので、受け手側でキャストする必要があるが、そのキャストにはそれなりにコストがかかるらしいので、そのコストを支払うかどうかの判断は受け手側に任せた方が良いだろうと考えた。

--- libsources/WavAgentErrorCode.h
+++ libsources/WavAgentErrorCode.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 #include <iostream>
-#include <string>
+#include <string_view>
 
 namespace wavAgent
 {
@@ -18,10 +18,10 @@ namespace wavAgent
     std::ostream &operator<<(std::ostream &os, WavAgentErrorCode code);
 
     // エラーコードを、その名称の文字列に変換する
-    std::string ConvertWavAgentErrorCodeToString(WavAgentErrorCode code);
+    std::string_view ConvertWavAgentErrorCodeToString(WavAgentErrorCode code);
 
     // エラーコードから、その原因を説明した文字列に変換する
-    std::string GetDescriptionOfErrorCode(WavAgentErrorCode code);
+    std::string_view GetDescriptionOfErrorCode(WavAgentErrorCode code);
 
     // WAvAgentErrorCodeを返す関数が正常に終了したかどうか判定する
     inline bool IsDataLibActionSucceeded(WavAgentErrorCode code)
