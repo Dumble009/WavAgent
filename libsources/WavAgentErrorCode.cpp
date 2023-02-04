4b262bc4f0dc4bbd14ae9a0e0880107c688b7e0e
Dumble009 <daichi19990821@gmail.com>
2023-02-04 13:33:51 +0900

    WavAgentErrorCode.cppを実装。テストが通った。
    前回コミットの、文字列を返す関数の型をstringかたstring_viewに変えたことへの対応を行った。

--- libsources/WavAgentErrorCode.cpp
+++ libsources/WavAgentErrorCode.cpp
@@ -4,16 +4,34 @@ namespace wavAgent
 {
     std::ostream &operator<<(std::ostream &os, WavAgentErrorCode code)
     {
-        return os;
+        return (os << ConvertWavAgentErrorCodeToString(code));
     }
 
-    std::string ConvertWavAgentErrorCodeToString(WavAgentErrorCode code)
+    // WavAgentErrorCodeを文字列に変換した結果。
+    // WavAgentErrorCodeをint型に変換した時の要素番号の位置に、対応する文字列を入れる
+    constexpr std::string_view ERROR_CODE_NAMES[6] = {
+        "WAV_AGENT_SUCCESS",
+        "WAV_AGENT_FILE_NOT_EXIST",
+        "WAV_AGENT_NOT_WAV_FILE",
+        "WAV_AGENT_CHANNEL_OUT_OF_RANGE",
+        "WAV_AGENT_INVALID_FORMAT",
+        "WAV_AGENT_SOUND_DATA_IS_NOT_INITIALIZED"};
+
+    std::string_view ConvertWavAgentErrorCodeToString(WavAgentErrorCode code)
     {
-        return "";
+        return ERROR_CODE_NAMES[(int)code];
     }
 
-    std::string GetDescriptionOfErrorCode(WavAgentErrorCode code)
+    constexpr std::string_view ERROR_DESCRIPTIONS[6] = {
+        "WavAgent : Operation successfully completed.",
+        "WavAgent : Loading a file failed. A file does not exist.",
+        "WavAgent : Loading a file failed. A file is not a wav file.",
+        "WavAgent : Invalid channel index was passed. Please check a channel count in a meta data.",
+        "WavAgent : Claimed format does not match with a format of data. Please check a sample format in a meta data.",
+        "WavAgent : A sound data was not initialized. Please initialize it using Load function before accessing its members."};
+
+    std::string_view GetDescriptionOfErrorCode(WavAgentErrorCode code)
     {
-        return "";
+        return ERROR_DESCRIPTIONS[(int)code];
     }
 }
