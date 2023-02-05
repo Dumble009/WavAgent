7a2f99bb692ce4235148cdfd98f8d86f10cb11ff
Dumble009 <daichi19990821@gmail.com>
2023-02-05 12:23:40 +0900

    MetaDataクラスを実装
    MetaDataクラスの中身を実装した。
    まだテストは通していないので、今後実装が変化する可能性はある。

--- libsources/MetaData.cpp
+++ libsources/MetaData.cpp
@@ -8,30 +8,38 @@ namespace wavAgent
         int _samplingFreqHz,
         SampleFormatType _sampleFormat,
         int _sampleCount)
+        : channelCount(_channelCount),
+          samplingFreqHz(_samplingFreqHz),
+          sampleFormat(_sampleFormat),
+          sampleCount(_sampleCount)
     {
     }
 
     // 波形データに含まれるチャンネル数を返す
     WavAgentErrorCode MetaData::GetChannelCount(int &ret)
     {
+        ret = channelCount;
         return WavAgentErrorCode::WAV_AGENT_SUCCESS;
     }
 
     // サンプリング周波数。単位はHz
     WavAgentErrorCode MetaData::GetSamplingFreqHz(int &ret)
     {
+        ret = samplingFreqHz;
         return WavAgentErrorCode::WAV_AGENT_SUCCESS;
     }
 
     // サンプルのデータ形式
     WavAgentErrorCode MetaData::GetSampleFormat(SampleFormatType &ret)
     {
+        ret = sampleFormat;
         return WavAgentErrorCode::WAV_AGENT_SUCCESS;
     }
 
     // 波形データの1チャンネルに含まれるサンプル数
     WavAgentErrorCode MetaData::GetSampleCount(int &ret)
     {
+        ret = sampleCount;
         return WavAgentErrorCode::WAV_AGENT_SUCCESS;
     }
 }
